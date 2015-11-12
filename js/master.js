2791fcc7d289ea4e3982ef0fec1e9276c95c8da3
Tenkaklet <tenkaklet@gmail.com>
2015-11-12 22:26:20 +0100

    added

--- js/master.js
+++ js/master.js
@@ -1,27 +1,40 @@
 angular.module('InstagramApp', [])
 .controller('InstaController', ['$scope','$http', '$q', '$timeout', function ($scope, $http, $q, $timeout) {
 
-
+    $scope.showResults = false;
 
     $scope.pictures = [];
     $scope.InstagramSearch = function () {
-        $scope.query = true;
-        var tag = $scope.tagKeyword;
-        var baseURL = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?';
+            $timeout(function() {
+                money();
+            }, 1000);
+            $scope.query = true;
+            var tag = $scope.tagKeyword;
+            var baseURL = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?';
 
-        var params = {
-            client_id: '0115b5f0a43746a48c75c62e2fb78e72',
-            callback: 'JSON_CALLBACK',
-        };
-        $http.jsonp(baseURL, {params: params})
+            var params = {
+                client_id: '0115b5f0a43746a48c75c62e2fb78e72',
+                callback: 'JSON_CALLBACK',
+            };
+            function money () {
+                $scope.showResults = true;
+            $http.jsonp(baseURL, {params: params})
 
-        .then(function(response) {
-            $scope.pictures = response.data.data;
-            console.log($scope.pictures);
-            $scope.InstaForm.$setPristine();
-            $scope.query = false;
-        }, function (response) {
-            console.log('You failed!');
-        });
+
+                .then(function(response) {
+                    $scope.pictures = response.data.data;
+                    console.log($scope.pictures);
+                    $scope.InstaForm.$setPristine();
+                    $scope.query = false;
+                }, function (response) {
+                    console.log('You failed!');
+                });
+            }
+            resetKeyword();
+
     };
+
+    function resetKeyword () {
+        $scope.tagKeyword = $scope.tagKeyword;
+    }
 }]);
