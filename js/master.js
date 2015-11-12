fc311215eb736c45bf948f8576243c4b032e2af9
Tenkaklet <tenkaklet@gmail.com>
2015-11-12 17:56:28 +0100

    adding

--- js/master.js
+++ js/master.js
@@ -1,7 +1,19 @@
 angular.module('InstagramApp', [])
-.controller('InstaController', ['$scope','$http', '$q', function ($scope, $http, $q) {
+.controller('InstaController', ['$scope','$http', '$q', '$timeout', function ($scope, $http, $q, $timeout) {
+
+
+    function wait() {
+    return $q(function(resolve, reject){
+      $timeout(function() {
+          resolve();
+  }, 80000000);
+    });
+}
+
     $scope.pictures = [];
     $scope.InstagramSearch = function () {
+        $scope.query = true;
+        wait();
         var tag = $scope.tagKeyword;
         var baseURL = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?';
 
@@ -10,10 +22,12 @@ angular.module('InstagramApp', [])
             callback: 'JSON_CALLBACK',
         };
         $http.jsonp(baseURL, {params: params})
+
         .then(function(response) {
             $scope.pictures = response.data.data;
             console.log($scope.pictures);
             $scope.InstaForm.$setPristine();
+            $scope.query = false;
         }, function (response) {
             console.log('You failed!');
         });
